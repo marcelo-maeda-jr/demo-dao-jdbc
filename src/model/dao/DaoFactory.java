ec5b0f42f035832e419f4da88a312a0b05620a7e
marcelo-maeda-jr <marcelo.yoshi.maeda@gmail.com>
2021-06-05 18:06:39 -0300

    findById implementation

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,6 @@
 package model.dao;
 
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
@@ -7,7 +8,7 @@ public class DaoFactory {
 	 * Aux class responsible for instantiating DAOs
 	 */
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC(DB.getConnection());
 	}
 	
 }
